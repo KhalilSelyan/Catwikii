de776d802057657d6365f663480c9721957f8198
Khalil Selyan <khalil.selyan@westerops.com>
2022-05-03 16:29:49 +0300

    favicon added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,16 @@ import { AppProps } from "next/app";
 import "../styles/index.css";
 import Header from "../components/header";
 import Footer from "../components/footer";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="flex flex-col font-montserrat">
+      <Head>
+        <title>CatWiki</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/catLogox.ico" />
+      </Head>
       <Header />
       <Component {...pageProps} />
       <div className="mt-auto">
--- pages/mostSearched.tsx
+++ pages/mostSearched.tsx
@@ -1,5 +1,4 @@
 import axios from "axios";
-import Image from "next/image";
 import React from "react";
 
 let top10 = [
